b03e0afc25ceebf20bb02824ad86b3103540a2ab
Neel <nkachalia1@gmail.com>
2023-09-28 13:32:57 -0400

    initialized cats

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,8 +1,8 @@
 class Cat < ApplicationRecord
-    validates :name, presence: true, null: false
-    validates :color, null: false, inclusion: { in: CAT_COLORS, message: "is not a valid color" }
-    validates :sex, null: false, inclusion: { in ["M", "F"], message: "must be 'M' or 'F'" }
     CAT_COLORS = ["Black", "White", "Gray", "Orange", "Calico"].freeze
+    validates :name, presence: true
+    validates :color, presence: true, inclusion: { in: CAT_COLORS, message: "is not a valid color" }
+    validates :sex, presence: true, inclusion: { in: ["M", "F"], message: "must be 'M' or 'F'" }
     validates :description, presence: true
     validates :timestamps, presence: true
 
@@ -14,19 +14,12 @@ class Cat < ApplicationRecord
 
     validate :birth_date_cannot_be_future
 
-    To calculate the age of a cat based on its birth_date in a Ruby on Rails model, you can define an #age method. Here's an example of how you can implement this method in your Cat model:
-
-ruby
-
-class Cat < ApplicationRecord
-  # Other model code...
-
-  # Calculate the age based on birth_date
   def age
 
     # Calculate the age by subtracting the birth_date from today's date
     today = Date.today
-    age_in_years = today.year - birth_date.year - (today.yday < birth_date.yday ? 1 : 0)
+    debugger
+    age_in_years = time_ago_in_words(self.birth_date)
 
     # You can also optionally include months and days for a more detailed age calculation
     age_in_months = today.month - birth_date.month
@@ -41,5 +34,8 @@ class Cat < ApplicationRecord
       "#{age_in_days} days"
     end
   end
+
+  validate :age
+
 
 end
